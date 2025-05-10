e7c51721be88fdffa9f0141028f0889f17995e64
AShigrev <merkrattrue@gmail.com>
2025-05-10 13:58:40 +0200

    решение задач 26_9077 егэ

--- 10_05_2025/26_9077.py
+++ 10_05_2025/26_9077.py
@@ -15,8 +15,9 @@ for i in data:
         scooters[i[3]].append(i[0] + i[1])
         counter_scooters += 1
     elif min(scooters[i[2]]) < i[0]:
-        scooters[i[2]].remove()
+        scooters[i[2]].remove(min(scooters[i[2]]))
         scooters[i[3]].append(i[0] + i[1])
     elif i[0] < min(scooters[i[2]]):
         counter_scooters += 1
         scooters[i[3]].append(i[0] + i[1])
+print(counter_scooters)
