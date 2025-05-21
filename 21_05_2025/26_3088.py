3078c60752e2173a54cf3d41aefe2c18735321bc
AShigrev <merkrattrue@gmail.com>
2025-05-21 17:49:01 +0200

    решение задачи 26_3088 егэ

---
+++ 21_05_2025/26_3088.py
@@ -0,0 +1,27 @@
+from logging import makeLogRecord
+
+with open('26_3088.txt') as file:
+    N = int(file.readline())
+    menu = dict()
+    for i in file:
+        time, burger_id = map(int, i.split())
+        if burger_id not in menu:
+            menu[burger_id] = []
+        menu[burger_id].append(time)
+for i in menu:
+    menu[i].sort()
+ans2 = []
+timeline = [0] * 961
+for burger_id in menu:
+    waiting = 0
+    for i in range(0, len(menu[burger_id])-1, 2):
+        start, end = menu[burger_id][i], menu[burger_id][i+1]
+        waiting += end - start
+        timeline[end] += 1
+    if len(menu[burger_id]) > 1:
+        waiting /= len(menu[burger_id]) // 2
+        ans2.append([waiting, burger_id])
+ans1 = 0
+for i in range(961):
+    ans1 = max(ans1, sum(timeline[i:i + 60]))
+print(ans1, max(ans2)[1])
