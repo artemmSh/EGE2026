f96ab4cf9b68306a333e0101d1a1529839ed88cf
artemmSh <merkrattrue@gmail.com>
2025-11-02 11:49:30 -0800

    хз

--- test/16.py
+++ test/16.py
@@ -1,8 +1,8 @@
 from sys import *
-setrecursionlimit(10000)
+setrecursionlimit(100000)
 def F(n):
     if n < 10:
         return n
     else:
         return 3*n + F(n-3)
-print(F(6250) + 2*F(6244)/F(6238))
+print((F(6250) + 2*F(6244))/F(6238))
--- test/2.py
+++ test/2.py
@@ -4,5 +4,5 @@ for w in (0, 1):
         for y in (0, 1):
             for z in (0, 1):
                 if y == 1 and z == 1 and w == 0:
-                    print(w, x, y, z)
+                    print(w, x, y, z, '|', 1)
 
