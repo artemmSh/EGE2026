a64584ff388ca44d3aa2367b2956d4ac0183c951
artemmSh <merkrattrue@gmail.com>
2026-02-09 18:03:17 +0200

    25 задачи егэ

--- task-25/task-635.py
+++ task-25/task-635.py
@@ -1,13 +1,12 @@
-def div(x):
-    res = set()
-    for i in range(2, int(x ** 0.5) + 1):
-        if x % i == 0:
-            res |= {i, x // i}
-    if len(res) == 3:
-        return max(res)
+def f(num):
+    dels = set()
+    for i in range(2, int(num ** .5) + 1):
+        if num % i == 0:
+            dels |= {i, num // i}
+    if len(dels) == 3:
+        return max(dels)
     return 0
 
-
-for N in range(int(106_732_567 ** 0.5), int(152_673_836 * 0.5)):
-    if M := div(N ** 2):
+for N in range(int(106_732_567 ** .5), int(152_673_836 ** .5)):
+    if M := f(N ** 2):
         print(N ** 2, M)
