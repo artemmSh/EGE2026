9b446f377c665c8497d56da9afcf157ef6b29f3e
artemmSh <merkrattrue@gmail.com>
2026-03-14 17:32:20 +0200

    это мясная 26 задача(не дорешано)

--- task-26/task-5066.py
+++ task-26/task-5066.py
@@ -8,7 +8,7 @@ cells = []
 while cases:
     cells.append([cases[0]])
     cases = cases[1:]
-    for case in cases:
+    for case in cases.copy():
         if cells[-1][-1] - case >= 7:
             cells[-1].append(case)
             cases.remove(case)
