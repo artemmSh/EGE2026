61fa24284bef0e6d96e77d4006f9e6736d7ef4b7
artemmSh <merkrattrue@gmail.com>
2026-04-30 17:57:37 +0200

    24

---
+++ task-24/2022/type-2/task-2942-replace.py
@@ -0,0 +1,10 @@
+with open(r'..\..\files\24_2942.txt') as file:
+    data = file.readline()
+
+for i in ['AB', 'AC']:
+    data = data.replace(i, '*')
+for i in 'ABC':
+    data = data.replace(i, ' ')
+data = data.split()
+
+print(len(max(data, key=len)))
